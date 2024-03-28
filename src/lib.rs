6c1e238ccbfac461972f1a588eca016be537846a
Utman <148863405+Rhyan2@users.noreply.github.com>
2024-03-28 16:09:26 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,43 @@
-#[derive(Debug)]
-pub struct ChessPosition;
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct ChessPosition{
+rank:i32,
+file:i32,
+}
+
+#[derive(Debug)]
+pub struct Queen{
+    position:ChessPosition,
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({rank}, {file}). If the position is invalid return None."
-        );
+        if (rank>=1 && rank<=8)&&(file>=1 &&file<=8){
+            return Some(ChessPosition{file,rank});
+        }else {
+            return None;
+        }
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!("Given the chess position {position:?}, construct a Queen struct.");
+    Queen{position}
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!("Determine if this Queen can attack the other Queen {other:?}");
+        if self.position.rank == other.position.rank{
+            return true;
+        }
+        if self.position.file==other.position.file{
+            return true;
+        }
+        if (self.position.rank - other.position.rank).abs()==(self.position.file - other.position.file).abs(){
+            return true;
+        }
+         false
+    
+
     }
+    
 }
